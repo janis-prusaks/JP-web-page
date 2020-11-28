9d9eb9be51df67da8ed71eeb5d4f5085a7292dc6
janis-prusaks <janim.prusakam@gmail.com>
2020-11-28 21:51:53 +0200

    papildināts ar navigāciju

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+window.addEventListener("hashchange", function () {
+    if (location.hash == "#start.html") {
+        console.log("test-start");
+        $("main").loadTemplate("start.html");
+    }
+    
+    if (location.hash == "#pieredze.html") {
+        console.log("test-pieredze");
+        $("main").loadTemplate("pieredze.html");
+    }
+    if (location.hash == "#pieredze2.html") {
+        console.log("test-pieredze2222");
+        $("main").loadTemplate("pieredze2.html");
+    }
+    if(location.hash == "#kontakti.html"){
+        console.log("test-kontakti");
+        $("main").loadTemplate("kontakti.html");
+
+
+    }
+});
+
+
